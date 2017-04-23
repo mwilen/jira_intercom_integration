f93c50c001bded759336f9727a6b98bbc36b0ae4
Mathias Wilén <mathias.wilen@gmail.com>
2017-04-23 18:53:08 +0200

    Coupled Intercom API with JIRA API so that they are linking back to eachother

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const util = require('util')
 
-const config = require('./config')
+const config = require('./config.js')
 
 JiraApi = require('jira').JiraApi;
 const Jira = new JiraApi('https', config.host, config.port, config.user, config.password, config.apiversion);
@@ -20,40 +20,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-const linkIntercom = (data) => {
-
-	let reply = {
-		id: '9025833924',
-		intercom_user_id: '71741',
-		body: 'Some reply :)',
-		type: 'admin',
-		message_type: 'note'
-	};
-
-	Intercom.conversations.reply(reply, (error, response) => {
-		if(error){
-			console.log(error)
-		}
-		else{
-			console.log(response)
-		}
-	});
-}
-linkIntercom()
-
 app.post('/api/createLink', (req, res, next) => {
+
+	let data = req.body;
+
+	console.log(data)
+
+	// Sample POST Data
+	// {
+	// 	"intercom": {
+	// 		"adminId": 71741,
+	// 		"conversationId": 9025833924,
+	// 		"url": "https://app.intercom.io/a/apps/vc7jxmzv/conversations/9025833924"
+	// 	},
+	// 	"jira": {
+	// 		"username": "admin",
+	// 		"project": "TEST",
+	// 		"type": "Bug",
+	// 		"priority": "Low",
+	// 		"summary": "This is not working properly",
+	// 		"description": "This is not working properly because this bug causes app to crash"
+	// 	}
+	// }
 
 	Jira.addNewIssue({
 		project: {
-			key: 'TEST'
+			key: data.jira.project
 		},
 		issuetype: {
-			name: 'Bug'
+			name: data.jira.type
 		},
-		summary: 'Lorem ipsum',
-		description: 'Lorem ipsum description',
+		priority: {
+			name: data.jira.priority
+		},
+		summary: data.jira.summary,
+		description: data.jira.description,
 		reporter: {
-			name: 'admin'
+			name: data.jira.username
 		}
 	},
 	(error, issue) => {
@@ -70,22 +73,44 @@ app.post('/api/createLink', (req, res, next) => {
 			let linkData = {
 				"relationship": "causes",
 			    "object": {
-			        "url" : "https://www.youtube.com/watch?v=G26HX51Xx5Q",
-			        "title": "Add getVersions and createVersion functions",
-					"summary": "There's an issue",
+			        "url" : data.intercom.url,
+			        "title": data.jira.summary,
+					//"summary": "There's an issue",
 			        "icon" : {
-			            "url16x16": "https://github.com/favicon.ico"
+			            "url16x16": "https://static.intercomassets.com/assets/favicon-d43b76a6a379bc237a54703bdb91d27a59b43929677efd6fbb722a005ea2a474.png"
 			        }
 			    }
 			};
 
 			Jira.createRemoteLink(issueKey, linkData, (err, link) => {
 				console.log('Status: ' + util.inspect(issue, false, null))
-				res.status(201).json('Link created')
 				linkIntercom({issue: issue.key, url: config.host + '/browse/' + issue.key})
 			});
 		}
 	})
+
+	const linkIntercom = (jira) => {
+
+		let note = {
+			id: data.intercom.conversationId,
+			admin_id: data.intercom.adminId,
+			body: 'Linked to JIRA Ticket #' + jira.issue + ': <a href="https://' + jira.url + '" target="_blank">' + data.jira.summary + '</a>',
+			type: 'admin',
+			message_type: 'note'
+		};
+
+		// Create note in Intercom
+		Intercom.conversations.reply(note, (error, response) => {
+			if(error){
+				console.log(error)
+				res.sendStatus(400)
+			}
+			else{
+				console.log(response)
+				res.status(201).json('Link created')
+			}
+		});
+	}
 })
 
 console.log('i am running')
