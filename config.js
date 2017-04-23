5065128ce626bfc18bb623fe459e7ac274cad8f5
Mathias Wilén <mathias.wilen@gmail.com>
2017-04-23 22:41:48 +0200

    Removed config from gitignore for now

---
+++ config.js
@@ -0,0 +1,22 @@
+const config = {
+	development: {
+		host: "mwilen.atlassian.net",
+		port: 443,
+		user: "mathias.wilen@gmail.com",
+		password: "kxcvwq4165",
+		apiversion: 2,
+		intercom_token: "dG9rOjRlNjIwMGMxXzgzMWVfNDViMl84OWRlXzE5ZjVhOWM0YWExODoxOjA="
+	},
+	production: {
+		host: process.env.JIRA_HOSTNAME,
+		port: 443,
+		user: process.env.JIRA_USER,
+		password: process.env.JIRA_PASSWORD,
+		apiversion: 2,
+		intercom_token: process.env.INTERCOM_ACCESS_TOKEN
+	}
+}
+
+exports.get = function get(env) {
+	return config[env] || config.development;
+}
