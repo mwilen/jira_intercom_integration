4ea2d68c6705aaedf55ef6482d37211b27825100
Mathias Wilén <mathias.wilen@gmail.com>
2017-05-02 21:25:24 +0200

    remove local config to test in production

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
 const config = {
-	development: {
-		host: "mwilen.atlassian.net",
-		port: 443,
-		user: "mathias.wilen@gmail.com",
-		password: "kxcvwq4165",
-		intercom_token: "dG9rOjRlNjIwMGMxXzgzMWVfNDViMl84OWRlXzE5ZjVhOWM0YWExODoxOjA="
-	},
 	production: {
 		host: process.env.JIRA_HOSTNAME,
 		port: 443,
